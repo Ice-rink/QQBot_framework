695823ccfdb6d0f06d6c76da6a06726f4d97b3bd
Ice-rink <15551057621@163.com>
2026-06-21 14:17:54 +0000

    refactor: 重构napcat插件逻辑

--- plugins/napcat.js
+++ plugins/napcat.js
@@ -1,22 +1,23 @@
-import { getBot } from "../lib/exports.js";
+import QQBot from "../lib/QQBot.js";
 import { logger } from "../lib/utils.js";
 
-export const metadata = {
+QQBot.metaData({
     name: "NapCat-Msg",
     version: "1.0.0",
     description: "返回一个假的NapCat信息"
-};
+});
 
-export default async function (QQBot) {
-    const { logger, addCmd, cmd, reply } = QQBot;
+QQBot.on("message.c2c", onSend);
+QQBot.on("message.group", onSend);
 
-    QQBot.on("message.c2c", (pack) => {
-        // logger.info(JSON.stringify(pack, null, 4));
-        if (pack.content.includes("#napcat")) reply(pack, [
-            "NapCat 信息",
-            "版本: 1.14.514",
-            "平台: linux(128 - bit)",
-            "运行时间: 114514天 1919小时 810分钟"
-        ].join("\n"))
-    })
+function onSend(pack, reply) {
+    // logger.info(JSON.stringify(pack, null, 4))
+    if (!pack.content.includes("#napcat")) return;
+
+    reply([
+        "NapCat 信息",
+        "版本: 1.14.514",
+        "平台: linux(128 - bit)",
+        "运行时间: 114514天 1919小时 810分钟"
+    ].join("\n"))
 }
